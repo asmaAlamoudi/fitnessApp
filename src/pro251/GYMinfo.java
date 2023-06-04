f8d022901504b92da171d2e6b0d1cd8bf5432e45
Reema Bahabri <45798388+Reema3bdo@users.noreply.github.com>
2023-06-04 19:23:32 +0300

    Add files via upload

---
+++ src/pro251/GYMinfo.java
@@ -0,0 +1,72 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkg251project;
+
+
+public class GYMinfo {
+    
+    String gymName;
+    String gymDescription;
+    String gymLocation;
+    int gymPrice;
+
+    GYMinfo() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public String getGymName() {
+        return gymName;
+    }
+
+    public String getGymDescription() {
+        return gymDescription;
+    }
+
+    public String getGymLocation() {
+        return gymLocation;
+    }
+
+    public int getGymPrice() {
+        return gymPrice;
+    }
+
+    public void setGymName(String gymName) {
+        this.gymName = gymName;
+    }
+
+    public void setGymDescription(String gymDescription) {
+        this.gymDescription = gymDescription;
+    }
+
+    public void setGymLocation(String gymLocation) {
+        this.gymLocation = gymLocation;
+    }
+
+    public void setGymPrice(int gymPrice) {
+        this.gymPrice = gymPrice;
+    }
+
+    public GYMinfo(String gymName, String gymDescription, String gymLocation, int gymPrice) {
+        this.gymName = gymName;
+        this.gymDescription = gymDescription;
+        this.gymLocation = gymLocation;
+        this.gymPrice = gymPrice;
+    }
+
+    
+//    public void getBestPrice() {
+//       
+//        
+//    }
+
+    @Override
+    public String toString() {
+        return   "Name= " + gymName + ",\tDescription= " + gymDescription + ",\tLocation= " + gymLocation +" city"+ ",\tPrice= " + gymPrice +" SR";
+    }
+    
+    
+    
+}
